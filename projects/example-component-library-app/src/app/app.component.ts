c2ab3e41f64dd7028b6cc26245d20e8e46f857c4
Paramdeep Singh <prmdpsn56@gmail.com>
2024-09-05 19:42:42 -0700

    style: testing the changes for semantic versionning

--- projects/example-component-library-app/src/app/app.component.ts
+++ projects/example-component-library-app/src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { MatIconModule } from '@angular/material/icon';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormControl, FormGroupDirective, FormsModule, NgForm, ReactiveFormsModule, Validators} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import {ErrorStateMatcher} from '@angular/material/core';
 import { ButtonComponent, ExampleComponentLibraryComponent, ExampleComponentLibraryService, HeaderComponent, SampleComponent } from 'example-component-library';
 
 // Note:
@@ -18,15 +22,24 @@ import { ButtonComponent, ExampleComponentLibraryComponent, ExampleComponentLibr
 
 // Incorrect Reference
 // import { HeaderComponent } from '../../../example-component-library/src/public-api';
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ExampleComponentLibraryComponent, SampleComponent,ButtonComponent,HeaderComponent,HttpClientModule,MatIconModule],
+  imports: [RouterOutlet,ExampleComponentLibraryComponent, SampleComponent,ButtonComponent,HeaderComponent,HttpClientModule,MatIconModule,FormsModule, MatFormFieldModule, MatInputModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  matcher = new ErrorStateMatcher();
   title = 'example-component-library-app';
   constructor(private exampleComponentLibraryComponent:ExampleComponentLibraryService){
   }
